36ad1a7f8700a2e11703535a09c98950b82f988f
NARUSE, Yui <naruse@airemix.jp>
2011-07-10 15:55:53 +0900

    Fix extconf.rb as ruby's.

--- ext/json/ext/generator/extconf.rb
+++ ext/json/ext/generator/extconf.rb
@@ -1,15 +1,10 @@
 require 'mkmf'
 require 'rbconfig'
 
-if CONFIG['CC'] =~ /gcc/
-  $CFLAGS << ' -Wall -Wextra'
-  unless $CFLAGS.gsub!(/ -O[\dsz]?/, ' -O3')
-    $CFLAGS << ' -O3'
-  end
-end
-
-have_header("re.h")
-if RUBY_VERSION >= "1.9"
+if RUBY_VERSION < "1.9"
+  have_header("re.h")
+else
+  have_header("ruby/re.h")
   have_header("ruby/encoding.h")
 end
 create_makefile 'json/ext/generator'
--- ext/json/ext/parser/extconf.rb
+++ ext/json/ext/parser/extconf.rb
@@ -1,13 +1,10 @@
 require 'mkmf'
 require 'rbconfig'
 
-if CONFIG['CC'] =~ /gcc/
-  $CFLAGS << ' -Wall -Wextra'
-  unless $CFLAGS.gsub!(/ -O[\dsz]?/, ' -O3')
-    $CFLAGS << ' -O3'
-  end
+if RUBY_VERSION < "1.9"
+  have_header("re.h")
+else
+  have_header("ruby/re.h")
+  have_header("ruby/encoding.h")
 end
-
-have_header("re.h")
-have_header("ruby/st.h")
 create_makefile 'json/ext/parser'
